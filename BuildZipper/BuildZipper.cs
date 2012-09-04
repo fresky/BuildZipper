76aa7a5dbd839c35130366fe26cb2c4163999e63
fresky <dawei.xu@gmail.com>
2012-09-04 09:35:45 +0800

    first draft

---
+++ BuildZipper/BuildZipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildZipper
+{
+    class BuildZipper
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
